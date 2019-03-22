76a55fabf5ce285de4af915fbe62d7f3f65a150f
Julia Wopata <julia.wopata@gmail.com>
2019-03-21 21:02:01 -0500

    Completed app

--- Color Picker/ViewController.swift
+++ Color Picker/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var detailLabel: UILabel!
+    struct Color {
+        var color: String
+        var uiColor: UIColor
     }
 
+    var red = Color(color: "Red", uiColor: UIColor.red)
+    var orange = Color(color: "Orange", uiColor: UIColor.orange)
+    var yellow = Color(color: "Yellow", uiColor: UIColor.yellow)
+    var green = Color(color: "Green", uiColor: UIColor.green)
+    var blue = Color(color: "Blue", uiColor: UIColor.blue)
+    var purple = Color(color: "Purple", uiColor: UIColor.purple)
+    
+    private lazy var dataSource: [Color] = [red, orange, yellow, green, blue, purple]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        self.view.backgroundColor = dataSource[0].uiColor
+        detailLabel.text = dataSource[0].color
+    }
+}
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        detailLabel.text = dataSource[row].color
+        self.view.backgroundColor = dataSource[row].uiColor
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataSource[row].color
+    }
 }
 
