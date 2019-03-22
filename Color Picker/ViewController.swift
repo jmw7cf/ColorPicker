8c7efd750034e7aa206657f883d77e9f6fcac686
Julia Wopata <julia.wopata@gmail.com>
2019-03-21 21:25:04 -0500

    Set variable for initial index of color

--- Color Picker/ViewController.swift
+++ Color Picker/ViewController.swift
@@ -15,16 +15,19 @@ class ViewController: UIViewController {
     struct Color {
         var color: String
         var uiColor: UIColor
+        
+        init(color: String, uiColor: UIColor){
+            self.color = color
+            self.uiColor = uiColor
+        }
     }
-
-    var red = Color(color: "Red", uiColor: UIColor.red)
-    var orange = Color(color: "Orange", uiColor: UIColor.orange)
-    var yellow = Color(color: "Yellow", uiColor: UIColor.yellow)
-    var green = Color(color: "Green", uiColor: UIColor.green)
-    var blue = Color(color: "Blue", uiColor: UIColor.blue)
-    var purple = Color(color: "Purple", uiColor: UIColor.purple)
     
-    private lazy var dataSource: [Color] = [red, orange, yellow, green, blue, purple]
+    private var dataSource = [Color(color: "Red", uiColor: UIColor.red),
+                              Color(color: "Orange", uiColor: UIColor.orange),
+                              Color(color: "Yellow", uiColor: UIColor.yellow),
+                              Color(color: "Green", uiColor: UIColor.green),
+                              Color(color: "Blue", uiColor: UIColor.blue),
+                              Color(color: "Purple", uiColor: UIColor.purple)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +36,7 @@ class ViewController: UIViewController {
         let initialRow = 0
         self.view.backgroundColor = dataSource[initialRow].uiColor
         detailLabel.text = dataSource[initialRow].color
+        pickerView.selectedRow(inComponent: initialRow)
     }
 }
 
